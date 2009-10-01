814669559979435aa03c39cc0cd453c38e29640f
Jigar Gosar <jigar.gosar@gmail.com>
2009-10-01 08:40:31 +0530

    * renaming and text changes.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,13 +1,16 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
 
-function toggle_done() {
+function toggle_done_items() {
   $$(".done").each(function(e) {
     e.toggle()
   })
 }
 
 function toggle_marker_buttons(){
+   $$(".done").each(function(e) {
+    e.show()
+  })
   $$(".done_buttons").each(function(e) {
     e.toggle()
   })
