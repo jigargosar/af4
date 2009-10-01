17e1220cb9311e4e4678402334afe959b36cd9fe
Jigar Gosar <jigar.gosar@gmail.com>
2009-10-01 09:01:08 +0530

    * refactoring code reduction by using terse library alternatives.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -2,19 +2,9 @@
 // This file is automatically included by javascript_include_tag :defaults
 
 function toggle_done_items() {
-  $$(".done").each(function(e) {
-    e.toggle()
-  })
+  $$(".done").invoke("toggle")
 }
 
-function toggle_marker_buttons(){
-   $$(".done").each(function(e) {
-    e.show()
-  })
-  $$(".done_buttons").each(function(e) {
-    e.toggle()
-  })
-  $$(".marker_toggle_form").each(function(e) {
-    e.toggle()
-  })
+function toggle_marker_buttons() {
+  $$(".done_buttons, .marker_toggle_form").invoke("toggle")
 }
